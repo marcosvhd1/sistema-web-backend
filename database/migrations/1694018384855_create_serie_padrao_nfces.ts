51f14ff6c84b9e85740e9bcbcb32bc554fc25421
Vini <marcosvhd1@gmail.com>
2023-09-06 14:23:31 -0300

    novo campo serie_nfce

--- database/migrations/1694018384855_create_serie_padrao_nfces.ts
+++ database/migrations/1694018384855_create_serie_padrao_nfces.ts
@@ -1,11 +1,11 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'produtos'
+  protected tableName = 'configs'
 
   public async up () {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string('nprod').alter();
+      table.string('serie_padrao_nfce');
     })
   }
 
