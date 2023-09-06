79209e44d246dae277f36902efe8150d10528089
Vini <marcosvhd1@gmail.com>
2023-09-06 15:18:30 -0300

    correções migration down

--- database/migrations/1693847567428_alter_nprod_types.ts
+++ database/migrations/1693847567428_alter_nprod_types.ts
@@ -6,10 +6,12 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.alterTable(this.tableName, (table) => {
       table.string('nprod').alter();
-    })
+    });
   }
 
   public async down () {
-    this.schema.dropTable(this.tableName);
+    this.schema.alterTable(this.tableName, (table) => {
+      table.integer('nprod').alter();
+    });
   }
 }
--- database/migrations/1694018384855_create_serie_padrao_nfces.ts
+++ database/migrations/1694018384855_create_serie_padrao_nfces.ts
@@ -6,10 +6,12 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.alterTable(this.tableName, (table) => {
       table.string('serie_padrao_nfce');
-    })
+    });
   }
 
   public async down () {
-    this.schema.dropTable(this.tableName);
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('serie_padrao_nfce');
+    });
   }
 }
