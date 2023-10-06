285d79e247242aca97c3bb006b9205cdc24c0f96
Vinicius <marcosvhd1@gmail.com>
2023-10-06 11:21:14 -0300

    relatorio gerencial

--- app/Controllers/Http/RelatoriosController.ts
+++ app/Controllers/Http/RelatoriosController.ts
@@ -4,6 +4,7 @@ import Database from '@ioc:Adonis/Lucid/Database';
 
 export default class RelatoriosController {
   public async generate({ request }: HttpContextContract) {
+    const estilo = request.input('estilo');
     const cliente = request.input('cliente');
     const cfop = request.input('cfop');
     const dataIni = request.input('dataIni');
@@ -19,30 +20,30 @@ export default class RelatoriosController {
     const id_emissor = request.input('id_emissor');
 
     try {
-      let whereSql = `id_emissor = ${id_emissor}`;
+      let whereSql = `notas.id_emissor = ${id_emissor}`;
 
-      if (cliente != '') whereSql += ` AND nome_destinatario like '%${cliente}%'`;
-      if (cfop != '') whereSql += ` AND cfop = '${cfop}'`;
-      if (dataIni != '' && dataFim != '') whereSql += ` AND data_emissao BETWEEN '${dataIni}' AND '${dataFim}'`;
+      if (cliente != '') whereSql += ` AND notas.nome_destinatario like '%${cliente}%'`;
+      if (cfop != '') whereSql += ` AND notas.cfop = '${cfop}'`;
+      if (dataIni != '' && dataFim != '') whereSql += ` AND notas.data_emissao BETWEEN '${dataIni}' AND '${dataFim}'`;
 
       //MODELO
       if (modelo55 && modelo65) null;
       else {
-        if (modelo55) whereSql += ' AND modelo = \'55\'';
-        else if (modelo65) whereSql += ' AND modelo = \'65\'';
+        if (modelo55) whereSql += ' AND notas.modelo = \'55\'';
+        else if (modelo65) whereSql += ' AND notas.modelo = \'65\'';
       }
 
       //TIPO
       if (entrada && saida) null;
       else {
-        if (entrada) whereSql += ' AND tipo = \'0\'';
-        else if (saida) whereSql += ' AND tipo = \'1\'';
+        if (entrada) whereSql += ' AND notas.tipo = \'0\'';
+        else if (saida) whereSql += ' AND notas.tipo = \'1\'';
       }
 
       //STATUS
       if (enviada && digitacao && cancelada && inutilizada) null;
       else if (enviada || digitacao || cancelada || inutilizada) {
-        whereSql += ' AND status IN (';
+        whereSql += ' AND notas.status IN (';
         
         if (enviada) whereSql += '\'Enviada\',';
         if (digitacao) whereSql += '\'Em digitação\',';
@@ -52,7 +53,18 @@ export default class RelatoriosController {
         whereSql += '\'\')';
       }
       
-      return await Database.from('notas').select('*').whereRaw(whereSql);
+      const notas = await Database.from('notas')
+        .select('*')
+        .whereRaw(whereSql);
+
+      if (estilo === '1') return notas;
+      else {
+        return await Database.from('notas')
+          .join('nf_produtos', 'notas.id', '=', 'nf_produtos.id_nfe')
+          .select('*')
+          .whereRaw(whereSql);
+      }
+      
     } catch (error: any) {
       throw new Exception(error);
     }
