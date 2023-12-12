90f30a2cd0bfdcca77395bac243a8c8d1a62d497
Vinícius <marcosvhd1@gmail.com>
2023-12-12 11:33:58 -0300

    correções de comunicação com o PHP

--- app/Controllers/Http/SefazController.ts
+++ app/Controllers/Http/SefazController.ts
@@ -7,13 +7,13 @@ export default class SefazController {
 
   public async preview({ request }: HttpContextContract) {
     const body = {
-      uuidNotaFiscal: request.input('id_nfe'),
-      uuidEmissor: request.input('id_emissor'),
+      idNF: request.input('id_nfe'),
+      idEmissor: request.input('id_emissor'),
       modelNF: request.input('model'),
     };
 
     try {
-      const response = await fetch('http://localhost:80/osmini-backend-php/RecebeDadosPreview.php', {
+      const response = await fetch('http://localhost:80/osmini-backend-php/nf-preview.php', {
         method: 'post',
         body: JSON.stringify(body),
         headers: {
@@ -31,11 +31,11 @@ export default class SefazController {
 
   public async status_servidor({ request }: HttpContextContract) {
     const body = { 
-      uuidEmissor: request.input('id_emissor'), 
+      idEmissor: request.input('id_emissor'), 
     };
 
     try {
-      const response = await fetch('http://localhost:80/osmini-backend-php/RecebeEventoStatusServidor.php', {
+      const response = await fetch('http://localhost:80/osmini-backend-php/status.php', {
         method: 'post',
         body: JSON.stringify(body),
         headers: {
@@ -53,13 +53,13 @@ export default class SefazController {
 
   public async emitir({ request }: HttpContextContract) {
     const body = {
-      uuidNotaFiscal: request.input('id_nfe'),
-      uuidEmissor: request.input('id_emissor'),
+      idNF: request.input('id_nfe'),
+      idEmissor: request.input('id_emissor'),
       modelNF: request.input('model'),
     };
 
     try {
-      const response = await fetch('http://localhost:80/osmini-backend-php/RecebeDadosEmissao.php', {
+      const response = await fetch('http://localhost:80/osmini-backend-php/nf-emitir.php', {
         method: 'post',
         body: JSON.stringify(body),
         headers: {
@@ -77,13 +77,14 @@ export default class SefazController {
 
   public async cancelar({ request }: HttpContextContract) {
     const body = {
-      uuidNotaFiscal: request.input('id_nfe'),
-      uuidEmissor: request.input('id_emissor'),
-      justificativa: request.input('justificativa'),
+      idNF: request.input('id_nfe'),
+      idEmissor: request.input('id_emissor'),
+      justify: request.input('justificativa'),
+      modelNF: request.input('model'),
     };
 
     try {
-      const response = await fetch('http://localhost:80/osmini-backend-php/RecebeDadosCancela.php', {
+      const response = await fetch('http://localhost:80/osmini-backend-php/nf-cancelar.php', {
         method: 'post',
         body: JSON.stringify(body),
         headers: {
@@ -101,15 +102,15 @@ export default class SefazController {
 
   public async inutilizar({ request }: HttpContextContract) {
     const body = {
-      uuidEmissor: request.input('id_emissor'),
-      justificativa: request.input('justificativa'),
+      serie: request.input('serie'),
+      justify: request.input('justificativa'),
+      idEmissor: request.input('id_emissor'),
       numeroIni: request.input('numero_inicial'),
       numeroFin: request.input('numero_final'),
-      serie: request.input('serie'),
     };
 
     try {
-      const response = await fetch('http://localhost:80/osmini-backend-php/RecebeDadosInutilizar.php', {
+      const response = await fetch('http://localhost:80/osmini-backend-php/nf-inutilizar.php', {
         method: 'post',
         body: JSON.stringify(body),
         headers: {
@@ -127,14 +128,14 @@ export default class SefazController {
 
   public async cce({ request }: HttpContextContract) {
     const body = {
+      idNF: request.input('id_nfe'),
       correcao: request.input('correcao'),
-      uuidNotaFiscal: request.input('id_nfe'),
-      uuidEmissor: request.input('id_emissor'),
+      idEmissor: request.input('id_emissor'),
       numeroSeqEvento: request.input('seq_evento'),
     };
 
     try {
-      const response = await fetch('http://localhost:80/osmini-backend-php/RecebeDadosCCE.php', {
+      const response = await fetch('http://localhost:80/osmini-backend-php/nf-cce.php', {
         method: 'post',
         body: JSON.stringify(body),
         headers: {
