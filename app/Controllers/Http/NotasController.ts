13d31cc0f645e84bcc525026ef76c09a3d56a798
Vinícius <marcosvhd1@gmail.com>
2023-12-13 11:23:02 -0300

    correção no cadastro e alteração da nf

--- app/Controllers/Http/NotasController.ts
+++ app/Controllers/Http/NotasController.ts
@@ -39,7 +39,7 @@ export default class NotasController {
     const filterDate = request.input('filter_date');
     const dataInicial = request.input('data_inicial');
     const filterStatus = request.input('filter_status');
-    
+
     const whereStatus = ` and status = '${filterStatus}'`;
     const whereFilter = ` and ${filter}::TEXT ilike '%${description.toUpperCase()}%'`;
     const whereDate = ` and ${filterDate} between '${dataInicial}' and '${dataFinal}'`;
@@ -78,80 +78,21 @@ export default class NotasController {
   }
 
   public async create({ request }: HttpContextContract) {
+    let body = request.body();
+
     try {
-      const data = {
-        'id_emissor': request.input('id_emissor'),
-        'id_destinatario': request.input('destinatario.id'),
-        'nome_destinatario': request.input('destinatario.razao'),
-        'cod': request.input('cod'),
-        'serie': request.input('serie'),
-        'natureza_operacao': request.input('natureza_operacao'),
-        'cfop': request.input('cfop'),
-        'status': request.input('status'),
-        'tipo': request.input('tipo'),
-        'finalidade': request.input('finalidade'),
-        'modelo': request.input('modelo'),
-        'consumidor_final': request.input('consumidor_final'),
-        'data_emissao': request.input('data_emissao'),
-        'data_saida': request.input('data_saida'),
-        'hora': request.input('hora'),
-        'base_calc_icms': request.input('base_calc_icms'),
-        'total_icms': request.input('total_icms'),
-        'base_icms_st': request.input('base_icms_st'),
-        'total_icms_st': request.input('total_icms_st'),
-        'total_frete': request.input('total_frete'),
-        'valor_seguro': request.input('valor_seguro'),
-        'outras_despesas': request.input('outras_despesas'),
-        'total_ipi': request.input('total_ipi'),
-        'total_ii': request.input('total_ii'),
-        'total_pis': request.input('total_pis'),
-        'total_cofins': request.input('total_cofins'),
-        'total_desconto_nf': request.input('total_desconto_nf'),
-        'total_produtos': request.input('total_produtos'),
-        'total_nota': request.input('total_nota'),
-        'total_fcp': request.input('total_fcp'),
-        'total_fcp_st': request.input('total_fcp_st'),
-        'aliquota_credito': request.input('aliquota_credito'),
-        'valor_credito': request.input('valor_credito'),
-        'retencao_pis': request.input('retencao_pis'),
-        'retencao_cofins': request.input('retencao_cofins'),
-        'retencao_csll': request.input('retencao_csll'),
-        'base_calc_irrf': request.input('base_calc_irrf'),
-        'retencao_irrf': request.input('retencao_irrf'),
-        'base_prev_social': request.input('base_prev_social'),
-        'ret_prov_social': request.input('ret_prov_social'),
-        'partilha_icms_dest': request.input('partilha_icms_dest'),
-        'partilha_icms_rem': request.input('partilha_icms_rem'),
-        'fcp_uf_dest': request.input('fcp_uf_dest'),
-        'total_ipi_devolvido': request.input('total_ipi_devolvido'),
-        'presenca_comprador': request.input('presenca_comprador'),
-        'ind_intermed': request.input('ind_intermed'),
-        'cnpj_intermed': request.input('cnpj_intermed'),
-        'id_intermed': request.input('id_intermed'),
-        'modalidade_frete': request.input('modalidade_frete'),
-        'id_transportadora': request.input('transportadora.id'),
-        'quantidade_transporte': request.input('quantidade_transporte'),
-        'especie_transporte': request.input('especie_transporte'),
-        'marca_transporte': request.input('marca_transporte'),
-        'numero_transporte': request.input('numero_transporte'),
-        'peso_bruto': request.input('peso_bruto'),
-        'peso_liquido': request.input('peso_liquido'),
-        'info_adicionais': request.input('info_adicionais'),
-        'fonte_valor_aprox_tributos': request.input('fonte_valor_aprox_tributos'),
-        'ecf_referenciado': request.input('ecf_referenciado'),
-        'n_coo': request.input('n_coo'),
-        'chave_acesso': request.input('chave_acesso'),
-        'protocolo': request.input('protocolo'),
-        'uf_embarque': request.input('uf_embarque'),
-        'local_embarque': request.input('local_embarque'),
-        'local_despacho': request.input('local_despacho'),
-        'uf_saida': request.input('uf_saida'),
-        'local_saida': request.input('local_saida'),
-        'num_di': request.input('num_di'),
-        'data_di': request.input('data_di'),
+      body = {
+        ...body,
+        'id_destinatario': body.destinatario.id,
+        'nome_destinatario': body.destinatario.razao,
+        'id_transportadora': body.transportadora.id,
       };
-      
-      const result = await Nota.create(data);
+
+      delete body.destinatario;
+      delete body.produtos;
+      delete body.transportadora;
+
+      const result = await Nota.create(body);
 
       return result;
     } catch (error: any) {
@@ -160,81 +101,25 @@ export default class NotasController {
   }
 
   public async update({ request, params }: HttpContextContract) {
-    const body = {
-      'id_emissor': request.input('id_emissor'),
-      'id_destinatario': request.input('destinatario.id'),
-      'nome_destinatario': request.input('destinatario.razao'),
-      'cod': request.input('cod'),
-      'serie': request.input('serie'),
-      'natureza_operacao': request.input('natureza_operacao'),
-      'cfop': request.input('cfop'),
-      'status': request.input('status'),
-      'tipo': request.input('tipo'),
-      'finalidade': request.input('finalidade'),
-      'modelo': request.input('modelo'),
-      'consumidor_final': request.input('consumidor_final'),
-      'data_emissao': request.input('data_emissao'),
-      'data_saida': request.input('data_saida'),
-      'hora': request.input('hora'),
-      'base_calc_icms': request.input('base_calc_icms'),
-      'total_icms': request.input('total_icms'),
-      'base_icms_st': request.input('base_icms_st'),
-      'total_icms_st': request.input('total_icms_st'),
-      'total_frete': request.input('total_frete'),
-      'valor_seguro': request.input('valor_seguro'),
-      'outras_despesas': request.input('outras_despesas'),
-      'total_ipi': request.input('total_ipi'),
-      'total_ii': request.input('total_ii'),
-      'total_pis': request.input('total_pis'),
-      'total_cofins': request.input('total_cofins'),
-      'total_desconto_nf': request.input('total_desconto_nf'),
-      'total_produtos': request.input('total_produtos'),
-      'total_nota': request.input('total_nota'),
-      'total_fcp': request.input('total_fcp'),
-      'total_fcp_st': request.input('total_fcp_st'),
-      'aliquota_credito': request.input('aliquota_credito'),
-      'valor_credito': request.input('valor_credito'),
-      'retencao_pis': request.input('retencao_pis'),
-      'retencao_cofins': request.input('retencao_cofins'),
-      'retencao_csll': request.input('retencao_csll'),
-      'base_calc_irrf': request.input('base_calc_irrf'),
-      'retencao_irrf': request.input('retencao_irrf'),
-      'base_prev_social': request.input('base_prev_social'),
-      'ret_prov_social': request.input('ret_prov_social'),
-      'partilha_icms_dest': request.input('partilha_icms_dest'),
-      'partilha_icms_rem': request.input('partilha_icms_rem'),
-      'fcp_uf_dest': request.input('fcp_uf_dest'),
-      'total_ipi_devolvido': request.input('total_ipi_devolvido'),
-      'presenca_comprador': request.input('presenca_comprador'),
-      'ind_intermed': request.input('ind_intermed'),
-      'cnpj_intermed': request.input('cnpj_intermed'),
-      'id_intermed': request.input('id_intermed'),
-      'modalidade_frete': request.input('modalidade_frete'),
-      'id_transportadora': request.input('transportadora.id'),
-      'quantidade_transporte': request.input('quantidade_transporte'),
-      'especie_transporte': request.input('especie_transporte'),
-      'marca_transporte': request.input('marca_transporte'),
-      'numero_transporte': request.input('numero_transporte'),
-      'peso_bruto': request.input('peso_bruto'),
-      'peso_liquido': request.input('peso_liquido'),
-      'info_adicionais': request.input('info_adicionais'),
-      'fonte_valor_aprox_tributos': request.input('fonte_valor_aprox_tributos'),
-      'ecf_referenciado': request.input('ecf_referenciado'),
-      'n_coo': request.input('n_coo'),
-      'chave_acesso': request.input('chave_acesso'),
-      'protocolo': request.input('protocolo'),
-      'uf_embarque': request.input('uf_embarque'),
-      'local_embarque': request.input('local_embarque'),
-      'local_despacho': request.input('local_despacho'),
-      'uf_saida': request.input('uf_saida'),
-      'local_saida': request.input('local_saida'),
-      'num_di': request.input('num_di'),
-      'data_di': request.input('data_di'),
-    };
+    let body = request.body();
 
     try {
+      body = {
+        ...body,
+        'id_destinatario': body.destinatario.id,
+        'nome_destinatario': body.destinatario.razao,
+        'id_transportadora': body.transportadora.id,
+      };
+
+      delete body.destinatario;
+      delete body.produtos;
+      delete body.transportadora;
+      delete body.forma_pagto;
+      delete body.duplicata;
+      delete body.chaves_ref;
+
       const result = await Nota.query().where('id', params.id).update(body);
-      
+
       return result;
     } catch (error: any) {
       throw new Exception(error);
